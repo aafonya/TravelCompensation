9f848bc63fce1d80137c11d1032556b4e5b3dfff
Judit Tolnai <Judit.Tolnai@kuka.com>
2023-03-05 08:40:26 +0100

    new pages added

--- TravelCompensationUI/Pages/Index.cshtml.cs
+++ TravelCompensationUI/Pages/Index.cshtml.cs
@@ -1,29 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TravelAllowance;
+using TravelAllowanceWebApp.Models;
 
-namespace TravelCompensationUI.Pages
+namespace TravelCompensationUI.Pages.NewFolder
 {
-   using TravelAllowance;
+    public class IndexModel : PageModel
+    {
+        private readonly ITravelCompensationService compensationService;
 
-   public class IndexModel : PageModel
-   {
-      private readonly ITravelCompensationService compensationService;
-      public IndexModel(ITravelCompensationService compensationService)
-      {
-         this.compensationService = compensationService;
-      }
-      public IQueryable<EmployeeRecord> Employees { get; set; }
+        public IndexModel(ITravelCompensationService compensationService)
+        {
+            this.compensationService = compensationService;
+        }
 
-      public async void OnGet()
-      {
-         var employeeData =  await compensationService.GetAllEmployeeData();
-         Employees = employeeData.AsQueryable();
-      }
+        public List<EmployeeRecord> Employee { get;set; } = default!;
 
-      public void OnPost()
-      {
-         var emailAddress = Request.Form["monthpicker"];
-         // do something with emailAddress
-      }
+        public async Task OnGetAsync()
+        {
+            //if (_context.Employee != null)
+            //{
+            //    Employee = await _context.Employee.ToListAsync();
+            //}
+            var employeeData = await compensationService.GetAllEmployeeData();
+            if (employeeData != null)
+            {
+                Employee = employeeData;
+            }
+            
+        }
     }
 }
