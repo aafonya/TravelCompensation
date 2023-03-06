630142b52554bef27f6aa4f9b9dae54ac3448040
Judit Tolnai <Judit.Tolnai@kuka.com>
2023-03-06 03:05:35 +0100

    modify data handling

--- TravelAllowance/Model/EmployeeRecord.cs
+++ TravelAllowance/Model/EmployeeRecord.cs
@@ -2,16 +2,14 @@
 {
    public record EmployeeRecord
    {
-      public EmployeeRecord(string name, TravelAttributesRecord travelattr, List<DateTime> workDays)
+      public EmployeeRecord(string name, TravelAttributesRecord travelattr)
       {
          Name = name;
          TravelAttribute = travelattr;
-         WorkDays = workDays;
       }
 
       public string Name { get; }
 
       public TravelAttributesRecord TravelAttribute { get; set; }
-      public List<DateTime> WorkDays { get; set; }
    }
 }
